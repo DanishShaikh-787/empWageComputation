68927f2d868068ae585ffacd87e6fd93ebb9b86d
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-09 14:06:26 +0530

    Added : Empolyee Part Time and Full Time Salary in For Loop

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -3,19 +3,24 @@
 is_FullTime=1
 is_PartTime=2
 emp_Rate_Per_Hour=20
+total_Salary=0
+num_Of_Working_Days=20
 
-random_Check=$((RANDOM%3))
-case $random_Check in
-        $is_FullTime ) 
-                emp_Hrs=8
-                ;;
-        $is_PartTime )
-                emp_Hrs=4
-                ;;
-        * )
-                emp_Hrs=0
-                ;;
-esac
-
-salary=$(($emp_Rate_Per_Hour*$emp_Hrs))
-echo "Your One Day Salary Is : " $salary
+for (( day=1; day<=$num_Of_Working_Days; day++ ))
+do
+	random_Check=$((RANDOM%3))
+	case $random_Check in
+        	$is_FullTime ) 
+                	emp_Hrs=8
+                	;;
+        	$is_PartTime )
+                	emp_Hrs=4
+                	;;
+        	* )
+                	emp_Hrs=0
+                	;;
+	esac
+	salary=$(($emp_Rate_Per_Hour*$emp_Hrs))
+	total_Salary=$(($total_Salary+$salary))
+done
+echo "Your Total Salary : " $total_Salary
