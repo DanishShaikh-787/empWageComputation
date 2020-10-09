23f2e6920b3a40408f3ed9a85119a996898cf93c
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-09 13:31:05 +0530

    Added : Empolyee One Day Salary

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,15 +1,18 @@
 #! /bin/bash -x 
 
-isPresent=1
-randomCheck=$((RANDOM%2))
+is_Present=1
+is_PartTime=2
+emp_Rate_Per_Hour=20
+random_Check=$((RANDOM%3))
 
-if [ $randomCheck -eq $isPresent ]
+if [ $random_Check -eq $is_Present ]
 then
-        empRatePerHour=20
-        empHrs=8
-        salary=$(($empRatePerHour*$empHrs))
+        emp_Hrs=8
+elif [ $random_Check -eq $is_PartTime ]
+then
+        emp_Hrs=4
 else
-        salary=0
+        emp_Hrs=0
 fi
-
+salary=$(($emp_Rate_Per_Hour*$emp_Hrs))
 echo "Your One Day Salary Is : " $salary
