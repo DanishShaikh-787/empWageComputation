f2fe5d628f3c028844fc141db34ed6c8bbb5e376
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-09 14:18:56 +0530

    Added : Empolyee Wage modify in Function with Array

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -x 
 
-#CONSTANTS FOR THE PROGRAM 
+#CONSTANT FOR THE PROGRAM
 IS_FULL_TIME=1
 IS_PART_TIME=2
 EMP_RATE_PER_HOUR=20
@@ -27,14 +27,22 @@ function getWorkingHours(){
 	echo $empHrs
 }
 
+function getEmpWage(){
+	oneDayHr=$1
+	echo $(($oneDayHr*$EMP_RATE_PER_HOUR))
+
+}
+
 while [[ $totalEmpHr -lt $MAX_HOURS_IN_MONTH && $totalWorkingDays -lt $NUM_OF_WORKING_DAYS ]]
 
 do
 	((totalWorkingDays++))
 	empHours=$( getWorkingHours $((RANDOM%3)) )
 	totalEmpHr=$(($totalEmpHr+$empHours))
-
+	dailyWages[$totalWorkingDays]=$( getEmpWage $empHours )
 done
 totalSalary=$(($EMP_RATE_PER_HOUR*$totalEmpHr))
 
 echo "Total Salary Is " $totalSalary
+
+echo "Daily Wages : " ${dailyWages[@]}
