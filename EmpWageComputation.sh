e5ce8c188689fb5407acbc7129e274543c66ea56
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-09 13:59:37 +0530

    Added : Empolyee Part Time and Full Time Salary in Case Statement

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,18 +1,21 @@
 #! /bin/bash -x 
 
-is_Present=1
+is_FullTime=1
 is_PartTime=2
 emp_Rate_Per_Hour=20
-random_Check=$((RANDOM%3))
 
-if [ $random_Check -eq $is_Present ]
-then
-        emp_Hrs=8
-elif [ $random_Check -eq $is_PartTime ]
-then
-        emp_Hrs=4
-else
-        emp_Hrs=0
-fi
+random_Check=$((RANDOM%3))
+case $random_Check in
+        $is_FullTime ) 
+                emp_Hrs=8
+                ;;
+        $is_PartTime )
+                emp_Hrs=4
+                ;;
+        * )
+                emp_Hrs=0
+                ;;
+esac
+
 salary=$(($emp_Rate_Per_Hour*$emp_Hrs))
 echo "Your One Day Salary Is : " $salary
